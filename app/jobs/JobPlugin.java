76f7e346853d677ba1affc3a5557d965c7c96df6
Seppel Hardt <sebasth@gmx.de>
2012-07-07 00:42:39 +0200

    cleaned some stuff in the JobPlugin.java

--- app/jobs/JobPlugin.java
+++ app/jobs/JobPlugin.java
@@ -1,22 +1,14 @@
 package jobs;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.text.ParseException;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 import play.Application;
 import play.Logger;
 import play.Plugin;
-import play.libs.Akka;
-import play.libs.Time.CronExpression;
 import akka.actor.Scheduler;
-import akka.util.Duration;
-import akka.util.FiniteDuration;
 
 /**
  * This is a job Plugin which loads all classes which are annotated with the
@@ -50,16 +42,11 @@ public class JobPlugin extends Plugin {
       try {
         final Class<?> forName = Class.forName(clazz, true, application.classloader());
         // if (forName.isAssignableFrom(AbstractJob.class) == false) {
-        // Logger.error("The class: " + clazz + " which is annotated with: " +
-        // AkkaJob.class.getName() + " is not of the type: " +
-        // AbstractJob.class.getName());
-        // continue;
-        // }
-        // instanziate the job
+
         final Class<AbstractJob> abstractJobClass = (Class<AbstractJob>) forName;
 
         final Constructor<AbstractJob> constructor = abstractJobClass.getConstructor();
-        final AbstractJob abstractJobInstance = constructor.newInstance();
+        constructor.newInstance();
 
       } catch (final NoSuchMethodException e) {
         Logger.error("Could not find default constructor with no parameters in: " + clazz, e);
