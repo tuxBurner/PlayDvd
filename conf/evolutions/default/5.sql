9aa7d688b912ac77ee5370fec3cb3c31983c73db
tuxburner <sebasth@gmx.de>
2013-02-05 00:07:57 +0100

    Added index on grabbertype and grabberid in the database so we can check if the movie was already grabbed in the database

--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -2,10 +2,13 @@
 ALTER TABLE  `movie` ADD  `imdb_id` VARCHAR( 25 ) NULL,
                      ADD  `grabber_id` VARCHAR( 25 ) NULL,
                      ADD  `grabber_type` VARCHAR( 15 ) default 'NONE' not null,
-                     ADD constraint ck_movie_grabber_type check (grabber_type in ('THETVDB','TMDB','NONE'))
+                     ADD constraint ck_movie_grabber_type check (grabber_type in ('THETVDB','TMDB','NONE'));
+CREATE index ix_movie_grabber_idtype on movie (grabber_id,grabber_type);
+
 
 
 # --- !Downs
-ALTER TABLE `user` DROP `imdb_id`,
+ALTER TABLE `movie` DROP `imdb_id`,
                    DROP `grabber_id`,
-                   DROP `grabber_type`
+                   DROP `grabber_type`;
+DROP index ix_movie_grabber_idtype on movie;
