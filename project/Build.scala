022d93980fd94afa93d2131b3286958c60e52b2c
tuxburner <sebasth@gmx.de>
2013-04-12 21:04:09 +0200

    Version 1.0

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "dvdDataBase"
-    val appVersion      = "1.0-SNAPSHOT"
+    val appVersion      = "1.0"
 
     val appDependencies = Seq(
 
