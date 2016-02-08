eccf4594113f3f7bb1342c596fbd722e81918d52
Sebastian Hardt <tuxburner@boggo.de>
2016-02-08 22:52:20 +0100

    Got js i18n running in static context

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,11 +2,18 @@ package controllers;
 
 
 import com.github.tuxBurner.jsAnnotations.JsRoutesComponent;
+import jsmessages.JsMessages;
 import jsmessages.JsMessagesFactory;
+import jsmessages.japi.Helper;
 import org.apache.commons.lang.ArrayUtils;
+import play.Play;
+import play.api.i18n.DefaultMessagesApi;
+import play.api.i18n.Messages;
+import play.libs.Scala;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import play.twirl.api.JavaScript;
 
 import javax.inject.Inject;
 
@@ -15,10 +22,14 @@ public class Application extends Controller
 {
 
   @Inject
-  JsMessagesFactory jsMessagesFactory;
+  JsRoutesComponent jsRoutesComponent;
+
+  static JsMessagesFactory jsMessagesFactory;
 
   @Inject
-  JsRoutesComponent jsRoutesComponent;
+  public Application(JsMessagesFactory jsMessagesFactory) {
+    Application.jsMessagesFactory = jsMessagesFactory;
+  }
 
 
   /**
@@ -52,16 +63,11 @@ public class Application extends Controller
    * @param keys
    * @return
    */
-  public static String getJsI8N(final String namespace, final String... keys)
+  public  static String getJsI8N(final String namespace, final String... keys)
   {
-
     final String[] allKeys = (String[]) ArrayUtils.addAll(GENERAL_I18N_JS_KEYS, keys);
-
-    //jsMessagesFactory.subset(keys);
-
-    //return JS_MESSAGES.subset(play.Play.application(), allKeys).generate(namespace).toString();
-    return "";
-
-
+    final JsMessages jsMessages = jsMessagesFactory.subset(Scala.varargs(allKeys));
+    final JavaScript jsScript = jsMessages.apply(Scala.Option("window.Messages"), Helper.messagesFromCurrentHttpContext());
+    return jsScript.text();
   }
 }
