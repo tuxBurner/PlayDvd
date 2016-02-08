c7796717c33d691f1f44cbea1f9eea85f66265e3
Sebastian Hardt <tuxburner@boggo.de>
2016-02-08 12:37:46 +0100

    Fixed som stuff

--- start-mysql.sh
+++ start-mysql.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name avdb-mysql -e MYSQL_ROOT_PASSWORD=avdb -e MYSQL_DATABASE=avdb -e MYSQL_USER=avdb -e MYSQL_PASSWORD=avdb -p 3306:3306 -d mysql:5.7
+docker run --name avdb-mysql -e MYSQL_ROOT_PASSWORD=mypw -e MYSQL_DATABASE=avdb -e MYSQL_USER=avdb -e MYSQL_PASSWORD=avdb -p 6606:3306 -d mysql/mysql-server:5.7
