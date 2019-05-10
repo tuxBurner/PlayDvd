96442c56df4d0aba4eb17130752efa497befe795
tuxBurner <sebasth@gmx.de>
2019-05-10 10:41:52 +0200

    #31 search is working now :)

--- app/grabbers/amazonwebcrawler/AmazonMovieWebCrawler.java
+++ app/grabbers/amazonwebcrawler/AmazonMovieWebCrawler.java
@@ -1,27 +1,23 @@
 package grabbers.amazonwebcrawler;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.typesafe.config.ConfigFactory;
 import grabbers.amazon.AmazonResult;
+import helpers.ConfigurationHelper;
 import jodd.http.HttpBrowser;
 import jodd.http.HttpRequest;
 import jodd.http.HttpResponse;
 import jodd.jerry.Jerry;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import play.Logger;
-import play.api.Play;
-import play.libs.ws.*;
 
-import javax.inject.Inject;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 
 /**
  * Crawls the amazon web page.
  * Normally we used the {@link grabbers.amazon.AmazonMovieLookuper} but there are now restriction how to use the partnernet api
  */
-public class AmazonMovieWebCrawler  {
+public class AmazonMovieWebCrawler {
 
 
   /**
@@ -34,6 +30,22 @@ public class AmazonMovieWebCrawler  {
    * Category to search in
    */
   private final static String AMAZON_CATEGOTY;
+
+
+  /**
+   * Map which contains the amazon copy type string -> internal copytype
+   */
+  private final static Map<String, String> AMAZON_COPY_TYPE_MAP = ConfigurationHelper.createValMap("dvdb.amazon.grabber.matchCopyType");
+
+  /**
+   * Map which contauns the amazon age rating -> internal age rating
+   */
+  private final static Map<String, String> AMAZON_AGE_RATING_MAP = ConfigurationHelper.createValMap("dvdb.amazon.grabber.matchAgeRating");
+
+  /**
+   * List of strings which are to remove from the title
+   */
+  private static List<String> AMAZON_REMOVE_FROM_TITLE = ConfigFactory.load().getStringList("dvdb.amazon.grabber.removeFromTitle");
 
   static {
     AMAZON_ENDPOINT_URL = ConfigFactory.load().getString("dvddb.amazon.webEndPoint");
@@ -52,7 +64,6 @@ public class AmazonMovieWebCrawler  {
   }
 
 
-
   /**
    * Finds dvd/bluray by name
    *
@@ -63,9 +74,9 @@ public class AmazonMovieWebCrawler  {
 
     final String url = AMAZON_ENDPOINT_URL + "/s";
     HttpBrowser browser = new HttpBrowser();
-    final Map<String,String> params = new HashMap<>();
-    params.put("k",name);
-    params.put("i",AMAZON_CATEGOTY);
+    final Map<String, String> params = new HashMap<>();
+    params.put("k", name);
+    params.put("i", AMAZON_CATEGOTY);
     HttpRequest request = HttpRequest.get(url)
       .query(params);
     final HttpResponse httpResponse = browser.sendRequest(request);
@@ -77,6 +88,7 @@ public class AmazonMovieWebCrawler  {
 
   /**
    * Extracts the results from the search page
+   *
    * @param pageContent the html content of the search page
    * @return the results
    */
@@ -92,22 +104,55 @@ public class AmazonMovieWebCrawler  {
 
     for (Jerry child : children) {
 
+      String title = child.$("h2 span").text();
+      if(CollectionUtils.isEmpty(AMAZON_REMOVE_FROM_TITLE) == false) {
+        for(final String removeFromTitle : AMAZON_REMOVE_FROM_TITLE) {
+          title = StringUtils.remove(title,removeFromTitle);
+        }
+      }
+      title = StringUtils.trim(title);
+
+      if(StringUtils.contains(child.html(),"<span class=\"a-size-base a-color-secondary\">Gesponsert</span>")) {
+        Logger.info("Skipping: "+title+" for search it is an affiliated content.");
+        continue;
+      }
+
       final String asin = child.attr("data-asin");
       final String imageUrl = child.$("[data-component-type='s-product-image'] img").attr("src");
-      final String title = child.$("h2 span").text();
+
+      final String ratingText = child.$(".a-icon-star-small .a-icon-alt").text();
+      final String rating =  (StringUtils.isBlank(ratingText)) ? "" :  StringUtils.split(ratingText, ' ')[0];
 
 
-      // DVD Blu-ray lookup
 
-      Logger.info("---------------------------------------------------------");
-      Logger.info(asin);
-      Logger.info(imageUrl);
-      Logger.info("---------------------------------------------------------");
+      // Gett all the types like dvd and bluray ...
+      final Jerry typeLinks = child.$("a.a-text-bold");
+      for (final Jerry typeLink: typeLinks) {
 
-      result.add(new AmazonResult(title,"4711","Type",asin,"Ean",new HashSet<>(),imageUrl));
+        final String typeText = typeLink.text().trim();
+        if(AMAZON_COPY_TYPE_MAP.containsKey(typeText) == false) {
+          Logger.info("Skipping amazon type: "+typeText+" for asin: "+asin+" the type was not configured in the type map");
+          continue;
+        }
+
+        String copyType = AMAZON_COPY_TYPE_MAP.get(typeText);
+        if(copyType.equals("BLURAY") == true && StringUtils.contains(title,"[Blu-ray 3D]") == true) {
+          copyType = "BLURAY3D";
+        }
+
+
+
+        Logger.info("---------------------------------------------------------");
+        Logger.info(asin);
+        Logger.info(imageUrl);
+        Logger.info(copyType);
+        Logger.info(rating);
+        Logger.info("---------------------------------------------------------");
+
+        result.add(new AmazonResult(title, rating, copyType, asin, "Ean", new HashSet<>(), imageUrl));
+      }
 
     }
-
 
 
     return result;
