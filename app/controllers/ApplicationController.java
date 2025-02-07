009f957991e5182096d19c9c93c9e962fe9b39c3
tuxBurner <s.hardt@micromata.de>
2025-02-07 11:54:54 +0100

    Fixed i18n messages

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -11,7 +11,6 @@ import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.mvc.Security;
-import play.twirl.api.JavaScript;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -21,58 +20,57 @@ import javax.inject.Singleton;
 public class ApplicationController extends Controller {
 
 
-    private static MessagesApi messagesApi;
+  private static MessagesApi messagesApi;
 
-    JsRoutesComponent jsRoutesComponent;
+  JsRoutesComponent jsRoutesComponent;
 
-    /**
-     * Array of keys which are in general use for js
-     */
-    private final static String[] GENERAL_I18N_JS_KEYS = {"btn.close"};
-
-    //final static JsMessages JS_MESSAGES = jsmessages.JsMessages.create(play.Play.application());
+  /**
+   * Array of keys which are in general use for js
+   */
+  private final static String[] GENERAL_I18N_JS_KEYS = {"btn.close"};
 
 
-    private static JsMessagesFactory jsMessagesFactory;
+  private static JsMessagesFactory jsMessagesFactory;
 
-    /**
-     * Constructor which gets the js message factory injected.
-     *
-     * @param jsMessagesFactory the js message factory
-     * @param jsRoutesComponent the component for the javascript routes
-     */
-    @Inject
-    public ApplicationController(final JsMessagesFactory jsMessagesFactory, final JsRoutesComponent jsRoutesComponent, final MessagesApi messagesApi) {
-        this.jsMessagesFactory = jsMessagesFactory;
-        this.jsRoutesComponent = jsRoutesComponent;
-        this.messagesApi = messagesApi;
-    }
+  /**
+   * Constructor which gets the js message factory injected.
+   *
+   * @param jsMessagesFactory the js message factory
+   * @param jsRoutesComponent the component for the javascript routes
+   */
+  @Inject
+  public ApplicationController(final JsMessagesFactory jsMessagesFactory, final JsRoutesComponent jsRoutesComponent, final MessagesApi messagesApi) {
+    this.jsMessagesFactory = jsMessagesFactory;
+    this.jsRoutesComponent = jsRoutesComponent;
+    this.messagesApi = messagesApi;
+  }
 
 
-    public Result index() {
-        return redirect(routes.ListCopiesController.listAllCopies());
-    }
+  public Result index() {
+    return redirect(routes.ListCopiesController.listAllCopies());
+  }
 
-    /**
-     * Register the routes to certain stuff to the javascript routing so we can
-     * reach it better from there
-     *
-     * @return
-     */
-    public Result jsRoutes(Http.Request request) {
-        return jsRoutesComponent.getJsRoutesResult(request);
-    }
+  /**
+   * Register the routes to certain stuff to the javascript routing so we can
+   * reach it better from there
+   *
+   * @return
+   */
+  public Result jsRoutes(Http.Request request) {
+    return jsRoutesComponent.getJsRoutesResult(request);
+  }
 
-    /**
-     * Gets the i18n text for the given keys and adds general keys
-     *     *
-     * @param keys
-     * @return
-     */
-    public static String getJsI8N(final Http.Request request, final String... keys) {
-        final String[] allKeys = (String[]) ArrayUtils.addAll(GENERAL_I18N_JS_KEYS, keys);
-        final JsMessages jsMessages = jsMessagesFactory.subset(Scala.varargs(allKeys));
-        //final JavaScript jsScript = jsMessages.apply(Scala.Option("window.Messages"), this.messagesApi.preferred(request));
-        return "TODO: LIFT";//jsScript.text();
-    }
+  /**
+   * Gets the i18n text for the given keys and adds general keys
+   * *
+   *
+   * @param keys
+   * @return
+   */
+  public static String getJsI8N(final Http.Request request, final String... keys) {
+    final String[] allKeys = ArrayUtils.addAll(GENERAL_I18N_JS_KEYS, keys);
+    final JsMessages jsMessages = jsMessagesFactory.subset(Scala.varargs(allKeys));
+    final var jsScript = jsMessages.apply(Scala.Option("window.Messages"), messagesApi.preferred(request).messages());
+    return jsScript.text();
+  }
 }
